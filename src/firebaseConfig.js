652b63d4042283b09c21a47cf5439e96b4a0e9b7
Panaddanakchukaew <6314110019@mut.ac.th>
2023-09-20 18:17:49 +0700

    chat

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getDatabase, ref, set, push } from 'firebase/database'
-
-
+import { getDatabase, ref, set, push, onValue } from 'firebase/database'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDYywuCpaspkz1cBi6YdAmYHB_mLll4bo4",
@@ -28,4 +26,4 @@ const auth = getAuth()
 const database = getDatabase(app)
 
 
-export { app, auth, database, ref, set, push }
+export { app, auth, database, ref, set, push, onValue }
